68dc1c065ece97241c312145bfb489d29c555100
Maks Fedak <maks.fedak334@gmail.com>
2026-05-19 20:41:38 +0300

    ф

--- 010_WeatherTask/Form1.Designer.cs
+++ 010_WeatherTask/Form1.Designer.cs
@@ -36,30 +36,33 @@
             TempLabel = new Label();
             label3 = new Label();
             label4 = new Label();
+            label5 = new Label();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(77, 22);
+            textBox1.Location = new Point(88, 29);
+            textBox1.Margin = new Padding(3, 4, 3, 4);
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(165, 23);
+            textBox1.Size = new Size(188, 27);
             textBox1.TabIndex = 0;
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(12, 25);
+            label1.Location = new Point(14, 33);
             label1.Name = "label1";
-            label1.Size = new Size(59, 15);
+            label1.Size = new Size(74, 20);
             label1.TabIndex = 1;
             label1.Text = "Write City";
             // 
             // button1
             // 
-            button1.Location = new Point(259, 22);
+            button1.Location = new Point(296, 29);
+            button1.Margin = new Padding(3, 4, 3, 4);
             button1.Name = "button1";
-            button1.Size = new Size(75, 23);
+            button1.Size = new Size(86, 31);
             button1.TabIndex = 2;
             button1.Text = "Searh";
             button1.UseVisualStyleBackColor = true;
@@ -68,17 +71,18 @@
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(697, 22);
+            label2.Location = new Point(797, 29);
             label2.Name = "label2";
-            label2.Size = new Size(71, 15);
+            label2.Size = new Size(88, 20);
             label2.TabIndex = 3;
             label2.Text = "My Weather";
             // 
             // pictureBox1
             // 
-            pictureBox1.Location = new Point(77, 165);
+            pictureBox1.Location = new Point(42, 220);
+            pictureBox1.Margin = new Padding(3, 4, 3, 4);
             pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(172, 133);
+            pictureBox1.Size = new Size(262, 208);
             pictureBox1.TabIndex = 4;
             pictureBox1.TabStop = false;
             // 
@@ -86,34 +90,45 @@
             // 
             TempLabel.AutoSize = true;
             TempLabel.Font = new Font("Segoe UI", 30F);
-            TempLabel.Location = new Point(357, 202);
+            TempLabel.Location = new Point(395, 267);
             TempLabel.Name = "TempLabel";
-            TempLabel.Size = new Size(130, 54);
+            TempLabel.Size = new Size(160, 67);
             TempLabel.TabIndex = 5;
             TempLabel.Text = "label3";
             // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(21, 128);
+            label3.Location = new Point(24, 171);
             label3.Name = "label3";
-            label3.Size = new Size(94, 15);
+            label3.Size = new Size(116, 20);
             label3.TabIndex = 6;
             label3.Text = "Current Weather";
             // 
             // label4
             // 
             label4.AutoSize = true;
-            label4.Location = new Point(697, 128);
+            label4.Location = new Point(797, 171);
             label4.Name = "label4";
-            label4.Size = new Size(0, 15);
+            label4.Size = new Size(0, 20);
             label4.TabIndex = 7;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(661, 276);
+            label5.Name = "label5";
+            label5.Size = new Size(50, 20);
+            label5.TabIndex = 8;
+            label5.Text = "label5";
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            BackColor = Color.FromArgb(255, 255, 192);
+            ClientSize = new Size(914, 600);
+            Controls.Add(label5);
             Controls.Add(label4);
             Controls.Add(label3);
             Controls.Add(TempLabel);
@@ -122,6 +137,7 @@
             Controls.Add(button1);
             Controls.Add(label1);
             Controls.Add(textBox1);
+            Margin = new Padding(3, 4, 3, 4);
             Name = "Form1";
             Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
@@ -139,5 +155,6 @@
         private Label TempLabel;
         private Label label3;
         private Label label4;
+        private Label label5;
     }
 }
--- 010_WeatherTask/Form1.cs
+++ 010_WeatherTask/Form1.cs
@@ -1,4 +1,5 @@
-using System.Data;
+using _010_WeatherTask.Xml2CSharp;
+using System.Xml.Serialization;
 
 namespace _010_WeatherTask
 {
@@ -13,12 +14,52 @@ namespace _010_WeatherTask
             label4.Text = DateTime.Now.ToShortDateString();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private async void button1_Click(object sender, EventArgs e)
         {
             string city = textBox1.Text;
             string uri = "https://api.openweathermap.org/data/2.5/weather?mode=xml&units=metric&q=city&appid=595b83742bd73770b24aaca75c5444a3";
 
+            try
+            {
+                string xmlResponse = await httpClient.GetStringAsync(uri);
+
+            
+                XmlSerializer serializer = new XmlSerializer(typeof(Current));
+                Current weatherData;
+
+                using (StringReader reader = new StringReader(xmlResponse))
+                {
+                    weatherData = (Current)serializer.Deserialize(reader);
+                }
+
+                Update(weatherData);
+            }
+            catch { }
             
         }
+        private void Update(Current data)
+        {
+            double tempVal = double.Parse(data.Temperature.Value, System.Globalization.CultureInfo.InvariantCulture);
+            TempLabel.Text = $"{Math.Round(tempVal)}°C";
+
+            string details = $"Real Feel: {data.Feels_like.Value}°\n" +
+                             $"Weather: {data.Weather.Value}\n" +
+                             $"Sunrise: {data.City.Sun.Rise}\n" +
+                             $"Sunset: {data.City.Sun.Set}";
+
+            label5.Text = details;
+
+            if (data.Weather != null && !string.IsNullOrEmpty(data.Weather.Icon))
+            {
+                string iconCode = data.Weather.Icon;
+             
+                string  iconUrl = $"https://openweathermap.org/img/wn/{iconCode}@2x.png";
+
+                pictureBox1.ImageLocation = iconUrl;
+                pictureBox1.SizeMode = PictureBoxSizeMode.Zoom; 
+            }
+           
+        }
+
     }
 }
